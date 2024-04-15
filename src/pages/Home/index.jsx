3e8c4caf127109b7a9e00a1af2d9a7eb2949ae8d
Kaio Giovanni <kaio.giovanni@gmail.com>
2024-04-14 21:39:30 -0300

    wip

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavbarMenu from "../../components/NavbarMenu";
 import Private from "../../components/Private";
+import api from "../../services/http_requests";
 
 const Home = () => {
+  const exchanges = ["BITSTAMP", "COINBASE", "KRAKEN"];
+  const [prices, setPrices] = useState({});
+
+  useEffect(() => {
+    console.log("Setting data...");
+    setData();
+  });
+
+  useEffect(() => {
+    console.log("Updating prices...");
+    console.table(prices);
+  }, [prices]);
+
+  const setData = () => {
+    getCryptoPrices("BTCUSD")
+      .then((response) => {
+        setPrices(response);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  async function getCryptoPrices(symbol) {
+    const prices = {};
+
+    for (const exchange of exchanges) {
+      try {
+        const response = await api.get(
+          `/v1/exchangerate/${symbol}?&exchange=${exchange}`
+        );
+        prices[exchange] = response.data.rate;
+      } catch (error) {
+        console.error(`Error fetching data from ${exchange}, error.message`);
+      }
+    }
+    return prices;
+  }
+
   return (
     <Private>
       <NavbarMenu />
